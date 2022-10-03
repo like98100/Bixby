559af9cf9f4755aa782a4dc301d2b443170cbaad
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-10-04 06:59:10 +0900

    스킬 구현 기초 및 원소 연산 시스템 기초
    스킬 Q, E 구현.
    원소 연산 시스템 List 형태로 구현.
    원소 컨트롤 스크립트 기초 구현.

---
+++ FaE-HTW/Assets/Scripts/ElementRule.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementRule : MonoBehaviour
+{
+    public static LinkedList<ElementType> Elements = new LinkedList<ElementType>();
+    public enum ElementType
+    {
+        NONE = -1,
+        FIRE = 0,
+        ICE = 1,
+        WATER = 2,
+        ELECTRICITY = 3,
+
+        NUM = 4,
+    };
+
+    public int CheckAdventage(ElementType myElementType, ElementType enemyElement) // 1일때 이김, -1일때 짐, 0일때 비김.
+    {
+        if (myElementType == ElementType.NONE) return 0; //속성이 없을때는 연산해봐야 의미가 없다.
+
+        if(Elements.Find(myElementType).Next == Elements.Find(enemyElement)
+            || Elements.Find(myElementType).Next == null)
+        {
+            return 1;
+        }
+        else if(Elements.Find(myElementType).Previous == Elements.Find(enemyElement)
+            || Elements.Find(myElementType).Previous == null)
+        {
+            return -1; // 일반 상태에선 져도 피해가 없는 것으로 끝나나, 쉴드에 질 때는 반사피해를 입는다.
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}

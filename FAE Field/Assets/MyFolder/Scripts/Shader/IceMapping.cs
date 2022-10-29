179e934019a03bd4fcfcc00a232ab12c48bb3110
jongmin2 <min980518@gmail.com>
2022-10-29 14:01:15 +0900

    디졸브 코루틴 적용
    디졸브 코루틴 적용

---
+++ FAE Field/Assets/MyFolder/Scripts/Shader/IceMapping.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceMapping : MonoBehaviour
+{
+    private ShaderManager shaderManager;
+
+    [SerializeField] private Shader iceShader;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        shaderManager = this.GetComponent<ShaderManager>();
+
+        GameObject[] iceObjects = GameObject.FindGameObjectsWithTag("iceObject");
+
+        shaderManager.ChangeShader(iceObjects, iceShader);
+    }
+}

035fc61fdaabdf5b8b4885a88cc6710860da324e
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2022-11-19 18:22:48 +0900

    각 던전 내 플레이어 및 UI 배치
    파티클 스크립트 조정사항 있음
    던전 내 카메라 조정사항
     심플 카메라 컨트롤러 해제
     캠 컨트롤 추가
    플레이어 위치 조정 필요
    게임매니저 프리팹

--- FAE prototype v.0.1.0/Assets/Bixby/Scripts/Quest/SetPositionParticle.cs
+++ FAE prototype v.0.1.0/Assets/Bixby/Scripts/Quest/SetPositionParticle.cs
@@ -10,8 +10,13 @@ public class SetPositionParticle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        positionParticle = GameObject.Find("PositionParticle");   
-        InitializeVariable();
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "FieldScene")
+        {//파티클 객체가 씬에 있으면 if문 제거해도 무관, 현재 씬에 파티클이 없어서 필드에서만 되도록 수정
+            positionParticle = GameObject.Find("PositionParticle");
+            InitializeVariable();
+        }
+        else
+            positionParticle = null;
     }
 
     // Update is called once per frame

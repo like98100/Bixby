85140c7dd91291ddbe550422fd48e1784ff37720
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2022-11-20 22:42:06 +0900

    미니맵 조정
    미니맵 카메라 크기 수정
    플레이어, 목적지 추가
    미니맵 객체 레이어 추가

--- FAE prototype v.0.1.0/Assets/Bixby/Scripts/UI/UI_Control.cs
+++ FAE prototype v.0.1.0/Assets/Bixby/Scripts/UI/UI_Control.cs
@@ -87,6 +87,9 @@ public class UI_Control : MonoBehaviour
                 case "Shop":
                     inventory.SetActive(true);
                     break;
+                //case "Map":
+                //    print("맵 열림");
+                //    break;열 때 맵 위치를 플레이어 중점으로 할 것
             }
             Cursor.lockState = CursorLockMode.None;
             aimPoint.SetActive(false);

ab489e44ce54b157818e8fd0169a1ef26ad6b48f
RyuCy <rcy1235@naver.com>
2022-11-26 05:45:24 +0900

    지난 파일 제거

--- Bixby_Enemy/Assets/Bixby/Scripts/Interface/IDamgeable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
- public interface IDamgeable
-{
-    void TakeHit(float damage);
-    void TakeElementHit(float damage, ElementRule.ElementType elementType);
-}

d4b316d9ef014f5752aab8e22c205e7c00975d90
RyuCy <rcy1235@naver.com>
2022-11-27 16:58:55 +0900

    중간 커밋
    FinalBoss
     - 패턴은 굴러가게만 하고 거리 조절 등 세부 설정은 넘긴 상태
    
    DungeonBoss
     - 패턴은 없이 껍데기만 만들어뒀음.
    
    Id
     - 일반 몬스터 3001>3000
     - 던전별 보스 각 3001~3004
     - 최종보스 3002 > 3005
    
    남은 것
     - 던전보스 패턴 추가
     - 원거리 몬스터 재가동

---
+++ FAE prototype v.0.2.0/Assets/Bixby/Scripts/Enemy/FinalBoss/FinalBoss.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FinalBoss : CombatStatus, IDamgeable
+{
+    public EnemyStatus Stat;
+
+    public LayerMask Mask = -1;
+    public LayerMask Ground = -1;
+    public GameObject Target;
+    public GameObject FireBall;
+    public GameObject WaterBall;
+    public GameObject WaterBallSpawner;
+
+    //public GameObject shield;
+    public Material[] mat;
+
+    public NavMeshAgent MyAgent;
+    public Animator Anim;
+    public Rigidbody rigid;
+    public SphereCollider col;
+
+    public float DealtDamage;
+    public bool isAttacked;
+
+    public float SkillCooldown;
+
+    private Shield shield;
+    [SerializeField] private bool setShield;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Stat = new EnemyStatus();
+        Stat = Stat.SetEnemyStatus(EnemyType.FinalBoss, ElementType.FIRE);
+
+        this.MyElement = Stat.element;
+        isAttacked = false;
+
+        SkillCooldown = 0.0f;
+
+        MyAgent = GetComponent<NavMeshAgent>();
+        Anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
+
+        shield = this.gameObject.transform.Find("Shield").GetComponent<Shield>();
+        shield.SetActive(true);
+        setShield = true;
+
+        //게이지 생성
+        UI_EnemyHp.EnemyHps.hpObjects.Add(Instantiate(UI_Control.Inst.EnemyHp.getPrefab(true), GameObject.Find("UI").transform.GetChild(1)));
+        UI_EnemyHp.EnemyHps.hpObjects[0].SetActive(false);
+        UI_EnemyHp.EnemyHps.shieldObject = Instantiate(UI_Control.Inst.EnemyHp.getPrefab(false), GameObject.Find("UI").transform.GetChild(1));
+        UI_EnemyHp.EnemyHps.shieldObject.SetActive(false);
+        UI_EnemyHp.EnemyHps.boss = this;
+    }
+
+    void FixedUpdate()
+    {
+        if (SkillCooldown > 0.0f)
+            SkillCooldown -= Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+        //rigid.AddForce(Vector3.down * 50.0f);
+        MyAgent.speed = Stat.moveSpeed * SpeedMultiply;
+        findPlayer(Stat.sight);
+
+        // if (isAttacked)
+        //     MyAgent.isStopped = true;
+        // else
+        //     MyAgent.isStopped = false;
+
+    }
+
+    private void findPlayer(float sight)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, sight, 
+                                                    Mask, QueryTriggerInteraction.Ignore);
+
+        if (colliders.Length > 0)
+        {    
+            Target = colliders[0].gameObject;
+        }
+    }
+
+    public void ChangePhase()
+    {
+        Material[] temp;
+
+        if ((int)Stat.element == (int)ElementType.FIRE)
+        {
+            Stat.element = ElementType.ICE;
+            this.MyElement = Stat.element;
+            
+            temp = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials;
+            temp[1] = mat[0];
+            transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials = temp;
+        }
+        else if ((int)Stat.element == (int)ElementType.ICE)
+        {
+            Stat.element = ElementType.WATER;
+            this.MyElement = Stat.element;
+            
+            temp = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials;
+            temp[1] = mat[1];
+            transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials = temp;
+        }
+        else if ((int)Stat.element == (int)ElementType.WATER)
+        {
+            Stat.element = ElementType.ELECTRICITY;
+            this.MyElement = Stat.element;
+
+            temp = transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials;
+            temp[1] = mat[2];
+            transform.GetChild(0).GetComponent<SkinnedMeshRenderer>().materials = temp;
+        }
+        SetBarrier();
+        SkillCooldown = 0.0f;
+    }
+
+    public void SetBarrier()
+    {
+        Stat.barrier = Stat.maxBarrier;
+        setShield = true;
+        shield.SetActive(true);
+        //col.enabled = true;
+    }
+
+    public virtual void TakeHit(float damage)
+    {
+        if (!setShield)
+            Stat.hp -= damage * AdditionalDamage;
+        else
+            Stat.barrier -= damage * AdditionalDamage;
+        UI_Control.Inst.damageSet((damage * AdditionalDamage).ToString(), this.gameObject);//대미지 UI 추가 코드
+        DealtDamage = Mathf.Round(damage * 10) * 0.1f;
+        
+        if (Stat.barrier <= 0.0f)
+        {
+            setShield = false;
+            shield.SetActive(false);
+            col.enabled = false;
+        }
+    }
+
+    public virtual void TakeElementHit(float damage, ElementRule.ElementType enemyElement) //���߿� ������ ���ɼ� ����.
+    {
+        setEnemyElement(enemyElement); // �̷��� EnemyElement�� �ٲ㼭 ���ų� enemyElement�״�� �ᵵ �ɵ�.
+        float curDamage = attackedOnNormal(damage);
+
+        if (EnemyElement != ElementType.NONE)
+        {
+            if (ElementStack.Count != 0)
+            {
+                if (EnemyElement != this.ElementStack.Peek())
+                {
+                    Debug.Log("Pushed!");
+                    this.ElementStack.Push(EnemyElement);
+                    checkIsPopTime();
+                }
+            }
+            else
+            {
+                Debug.Log("Pushed!");
+                this.ElementStack.Push(EnemyElement);
+                checkIsPopTime();
+            }
+        }
+
+        
+        Stat.hp -= curDamage * AdditionalDamage;
+        UI_Control.Inst.damageSet((curDamage * AdditionalDamage).ToString(), this.gameObject);//����� UI �߰� �ڵ�
+        DealtDamage = Mathf.Round(curDamage * 10) * 0.1f;
+    }
+
+    public void EndAttack()
+    {
+        isAttacked = false;
+    }
+
+    public void NormalAttackAreaCheck()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position+Vector3.forward*1.5f, Stat.attackRange, 
+                                                    Mask, QueryTriggerInteraction.Ignore);
+
+        if (colliders.Length > 0)
+        {    
+            setEnemyElement(colliders[0].GetComponent<PlayerContorl>().MyElement);
+            colliders[0].GetComponent<PlayerContorl>().TakeElementHit(Stat.damage, Stat.element);
+        }
+    }
+
+    public void DashAttackAreaCheck()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, Stat.attackRange*1.5f, 
+                                                    Mask, QueryTriggerInteraction.Ignore);
+
+        if (colliders.Length > 0)
+        {    
+            setEnemyElement(colliders[0].GetComponent<PlayerContorl>().MyElement);
+            colliders[0].GetComponent<PlayerContorl>().TakeElementHit(Stat.damage, Stat.element);
+        }
+    }
+
+    public void IceSkillAttackAreaCheck()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, Stat.attackRange*3.0f, 
+                                                    Mask, QueryTriggerInteraction.Ignore);
+
+        if (colliders.Length > 0)
+        {    
+            setEnemyElement(colliders[0].GetComponent<PlayerContorl>().MyElement);
+            colliders[0].GetComponent<PlayerContorl>().TakeElementHit(Stat.damage, Stat.element);
+        }
+    }
+
+}
---
+++ FAE prototype v.0.2.0/Assets/Bixby/Scripts/NPC/Mission.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    GameObject missionWindow;
+    UnityEngine.UI.Text missionTitle;
+    UnityEngine.UI.Text missionText;
+    void Awake()
+    {
+        missionWindow = GameObject.Find("Mission");
+        missionTitle = missionWindow.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>();
+        missionText = missionWindow.transform.GetChild(1).GetComponent<UnityEngine.UI.Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (missionTitle.text == "")
+            return;
+        if (UI_Control.Inst.OpenedWindow == null)
+            missionWindow.SetActive(true);
+        else
+            missionWindow.SetActive(!(UI_Control.Inst.OpenedWindow.name == "Inventory"
+                                    || UI_Control.Inst.OpenedWindow.name == "Shop"
+                                    || UI_Control.Inst.OpenedWindow.name == "Speech"
+                                    || UI_Control.Inst.OpenedWindow.transform.parent.gameObject.name == "COOK"));
+    }
+    public void misssionSet(string missionTitle, string missionText)
+    {
+        this.missionTitle.text = missionTitle;
+        this.missionText.text = missionText;
+    }
+}
